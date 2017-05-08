3147a43817086ac40856e5cb64863ffe1d259eda
Matt Ellis <matt.ellis@blubolt.com>
2017-05-08 17:29:35 +0100

    added some events

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,79 @@
 
 class ProductList extends React.Component {
+
+	// Pass a function to the product/child components props
+	handleProductUpVote(productId) {
+		console.log(productId + ' was upvoted');
+	}
+
     render() {
+		const products = Seed.products.sort((a, b) => (
+			b.votes - a.votes
+		));
+
+		// Map over the products array in seed.js it takes in a function and
+		// returns the props
+		const productComponents = products.map((product) => (
+            <Product
+				key={'product-' + product.id}
+				id={product.id}
+				title={product.title}
+				description={product.description}
+				url={product.url}
+				votes={product.votes}
+				submitterAvatarUrl={product.submitterAvatarUrl}
+				productImageUrl={product.productImageUrl}
+				onVote={this.handleProductUpVote}
+			/>
+		));
+
         return (
-            <div className="ui unstackable items">
-                <Product />
-            </div>
+			<div className='ui unstackable items'>
+				{productComponents}
+			</div>
         );
     }
 }
 
 class Product extends React.Component {
+
+	// when defining custom component methods, we have to manually bind
+	// this to the component
+	constructor(props) {
+		super(props);
+		this.handleUpVote = this.handleUpVote.bind(this);
+	}
+
+	handleUpVote() {
+		this.props.onVote(this.props.id)
+	}
+
     render() {
         return (
             <div className="item">
                 <div className="image">
-                    <img src="images/products/image-aqua.png" />
+                    <img src={this.props.productImageUrl} />
                 </div>
                 <div className="middle aligned content">
+					<div className="header">
+						<a onClick={this.handleUpVote}>
+							<i className="large caret up icon" />
+						</a>
+						{this.props.votes}
+					</div>
                     <div className="description">
-                        <a>Fort Knight</a>
-                        <p>Authentic renaissance actors, delivered in just two weeks</p>
+                        <a href={this.props.url}>
+							{this.props.title}
+						</a>
+                        <p>
+							{this.props.description}
+						</p>
                     </div>
                     <div className="extra">
                         <span>Submitted by:</span>
-                        <img 
-                            className="ui avatar image" 
-                            src="images/avatars/daniel.jpg"
+                        <img
+                            className="ui avatar image"
+                            src={this.props.submitterAvatarUrl}
                         />
                     </div>
                 </div>
