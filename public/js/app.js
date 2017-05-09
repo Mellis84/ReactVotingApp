1794b2db808249fb8fb20ffe11d7145201e0b5d6
Matt Ellis <matt.ellis@blubolt.com>
2017-05-09 17:16:19 +0100

    Finished app

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,49 @@
 
 class ProductList extends React.Component {
 
+	constructor(props) {
+		super(props);
+
+		this.state = {
+		  products: [],
+		};
+
+		this.handleProductUpVote = this.handleProductUpVote.bind(this);
+	}
+
+	componentDidMount() {
+		this.setState({ products: Seed.products });
+	}
+
 	// Pass a function to the product/child components props
 	handleProductUpVote(productId) {
-		console.log(productId + ' was upvoted');
+		const nextProducts = this.state.products.map((product) => {
+			// if current product matches product.id
+			if (product.id === productId) {
+				// copy over properties from original product object and overwrite
+				// votes with new object
+				return Object.assign({}, product, {
+					votes: product.votes + 1,
+				});
+			} else {
+				// if current product is not the one specified return unmodified product object
+				return product;
+			}
+		});
+		this.setState({
+			products: nextProducts,
+		});
 	}
 
     render() {
-		const products = Seed.products.sort((a, b) => (
-			b.votes - a.votes
-		));
+	    const products = this.state.products.sort((a, b) => (
+	      b.votes - a.votes
+	    ));
 
 		// Map over the products array in seed.js it takes in a function and
 		// returns the props
 		const productComponents = products.map((product) => (
+
             <Product
 				key={'product-' + product.id}
 				id={product.id}
@@ -40,7 +70,9 @@ class Product extends React.Component {
 	// when defining custom component methods, we have to manually bind
 	// this to the component
 	constructor(props) {
-		super(props);
+		super(props); // always call this first
+
+		// custom method bindings here
 		this.handleUpVote = this.handleUpVote.bind(this);
 	}
 
